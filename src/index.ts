2fb548cc2e5cc632a020907083bed2c7b3289dc0
hmsk <k.hamasaki@gmail.com>
2020-10-18 21:57:00 -0700

    Compile Elm with node-elm-compiler

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import { Plugin, Transform } from 'vite'
+//@ts-ignore
+import compiler from 'node-elm-compiler'
 
 const transform = (): Transform => {
   return {
     test: ({ path }) => path.endsWith('.elm'),
-    transform: ({ code }) => {
+    transform: async ({ code, path }) => {
+      const compiled = await compiler.compileToString([path], { output: '.js' })
       return {
-        code: code
+        code: `let output = {}; (function () { ${compiled} }).call(output); export default output.Elm;`
       }
     }
   }
