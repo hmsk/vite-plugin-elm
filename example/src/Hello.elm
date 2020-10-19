3280680d3a846540965dc1436b5f82c6c935b567
hmsk <k.hamasaki@gmail.com>
2020-10-18 22:50:47 -0700

    Update an example to use Browser.element instead of returning plain Html msg

--- example/src/Hello.elm
+++ example/src/Hello.elm
@@ -1,7 +1,37 @@
 module Hello exposing (main)
 
-import Html exposing (Html, text)
+import Browser
+import Html exposing (Html, text, div)
+import Html.Events exposing (onClick)
 
-main : Html msg
-main =
-    Html.text "I'm imported through vite-plugin-elm"
+main : Program String Model Msg
+main = Browser.element
+    { init = init
+    , update = update
+    , view = view
+    , subscriptions = subscriptions
+    }
+
+type alias Model =
+    { message: String
+    }
+
+init : String -> (Model, Cmd Msg)
+init flags = ({ message = "Hi, I'm compiled through vite-plugin-elm: " ++ flags }, Cmd.none)
+
+type Msg
+  = Name String
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+    case msg of
+        Name name ->
+            ({ model | message = name}, Cmd.none)
+
+view : Model -> Html Msg
+view model =
+    div [onClick (Name "Woooo")] [text model.message]
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+  Sub.none
--- example/src/main.js
+++ example/src/main.js
@@ -1,5 +1,6 @@
 import Elm from './Hello.elm'
 
 Elm.Hello.init({
-  node: document.getElementById('root')
+  node: document.getElementById('root'),
+  flags: "Initial Message"
 })
