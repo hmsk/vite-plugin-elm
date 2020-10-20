2aef5d03e91253afb0ade328d0ae317408413689
hmsk <k.hamasaki@gmail.com>
2020-10-19 22:01:18 -0700

    Optimize Elm compilations for production build

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,8 @@ import compiler from 'node-elm-compiler'
 const transform = (): Transform => {
   return {
     test: ({ path }) => path.endsWith('.elm'),
-    transform: async ({ code, path }) => {
-      const compiled = await compiler.compileToString([path], { output: '.js' })
+    transform: async ({ path, isBuild }) => {
+      const compiled = await compiler.compileToString([path], { output: '.js', optimize: isBuild, verbose: isBuild })
       return {
         code: `let output = {}; (function () { ${compiled} }).call(output); export default output.Elm;`
       }
