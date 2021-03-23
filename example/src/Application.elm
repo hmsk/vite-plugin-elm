1baa0981aa2d5e97c3469c06d468d4e380705a1e
hmsk <k.hamasaki@gmail.com>
2021-03-22 23:43:19 -0700

    Add an example for Browser.application

---
+++ example/src/Application.elm
@@ -0,0 +1,58 @@
+module Application exposing (..)
+
+import Browser
+import Html exposing (Html, div, h1, img, text)
+import Html.Attributes exposing (src)
+
+
+
+---- MODEL ----
+
+
+type alias Model =
+    {}
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( {}, Cmd.none )
+
+
+
+---- UPDATE ----
+
+
+type Msg
+    = NoOp
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    ( model, Cmd.none )
+
+
+
+---- VIEW ----
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ h1 [] [ text "Your Elm App is working!" ]
+        ]
+
+
+
+---- PROGRAM ----
+
+
+main : Program () Model Msg
+main =
+    Browser.application
+        { view = \model -> { title = "Test", body = [ view model ] }
+        , init = \_ _ _ -> ( {}, Cmd.none )
+        , update = \_ model -> ( model, Cmd.none )
+        , subscriptions = always Sub.none
+        , onUrlRequest = \_ -> NoOp
+        , onUrlChange = \_ -> NoOp
+        }
---
+++ example/src/application.js
@@ -0,0 +1,4 @@
+import { Elm } from './Application.elm'
+
+
+Elm.Application.init();
