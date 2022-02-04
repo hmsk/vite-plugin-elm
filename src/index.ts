f9c1dc4791d9b4968fb422d8dde56825fb97d42d
Emiliano Bovetti <emiliano.bovetti@prima.it>
2022-02-04 17:54:33 +0100

    Fix an issue by copying from elm-hot
    [elm-hot/resources/hmr.js:415](https://github.com/klazuka/elm-hot/blob/fb2dc49e9b4fa53b51fa6088a1ac7ffa0b72557a/resources/hmr.js#L415)

--- src/index.ts
+++ src/index.ts
@@ -283,7 +283,7 @@ if (import.meta.hot) {
           import.meta.hot.invalidate()
         }
 
-        if (typeof elmSymbol("elm$browser$Browser$application") !== "undefined" && typeof elmSymbol("elm$browser$Browser$Navigation") !== "undefined") {
+        if (typeof elmSymbol("elm$browser$Browser$application") !== "undefined") {
           const oldKeyLoc = findNavKey(oldModel)
           const newKeyLoc = findNavKey(newModel)
           let error = null
