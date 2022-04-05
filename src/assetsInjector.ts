9369e2ae3fda4112a9c7e4f1cb702ab933a595fb
hmsk <k.hamasaki@gmail.com>
2022-04-04 21:23:28 -0700

    support a helper to tag asset's path by elm-vite-plugin-helper

--- src/assetsInjector.ts
+++ src/assetsInjector.ts
@@ -3,6 +3,7 @@ import { parse } from 'acorn'
 import { fullAncestor as walk } from 'acorn-walk'
 
 const ASSET_TAG = /'\[VITE_PLUGIN_ELM_ASSET:(?<path>.+?)\]'/g
+const HELPER_PACKAGE_IDENTIFIER = 'VITE_PLUGIN_HELPER_ASSET'
 
 const importNameFrom = (path: string) => {
   return path.replace(/[/.\-~@?]/g, '_')
@@ -12,26 +13,93 @@ const generateImports = (paths: string[]) => {
   return paths.map((path) => `import ${importNameFrom(path)} from '${path}'`).join('\n')
 }
 
-interface NodeWithRaw extends Node {
+interface NodeLiteral extends Node {
   raw: string
+  value: string
 }
-const isLiteral = (node: Node): node is NodeWithRaw => node.type === 'Literal'
+const isLiteral = (node: Node): node is NodeLiteral => node.type === 'Literal'
+
+interface NodeDeclarator extends Node {
+  raw: string
+  id?: {
+    name?: string
+  }
+  init?: {
+    body?: {
+      body?: {
+        argument?: {
+          left?: {
+            raw?: string
+          }
+        }
+      }[]
+    }
+  }
+}
+const isDeclarator = (node: Node): node is NodeDeclarator => node.type === 'VariableDeclarator'
+
+interface NodeCallExpression extends Node {
+  callee?: {
+    name?: string
+  }
+  arguments?: (NodeLiteral | Node)[]
+}
+const isCallExpression = (node: Node): node is NodeCallExpression => node.type === 'CallExpression'
 
 const assetsInjector = (compiledElmCodeInEsm: string) => {
-  const taggedPaths: Record<string, { path: string; start: number; end: number }> = {}
+  const taggedPaths: {
+    path: string
+    start: number
+    end: number
+  }[] = []
 
+  const ast = parse(compiledElmCodeInEsm, { ecmaVersion: 2015, sourceType: 'module' })
+  let helperFunctionName: string
   walk(
-    parse(compiledElmCodeInEsm, { ecmaVersion: 2015, sourceType: 'module' }),
-    (node) => {
+    ast,
+    (node, state) => {
       if (isLiteral(node)) {
+        // Find callers of VitePluginHelper.asset
+        if (!helperFunctionName && node.raw === `'${HELPER_PACKAGE_IDENTIFIER}'`) {
+          const helperFunc = state?.find(
+            (nodeOnState): nodeOnState is NodeDeclarator =>
+              isDeclarator(nodeOnState) &&
+              nodeOnState?.init?.body?.body?.[0]?.argument?.left?.raw === `'${HELPER_PACKAGE_IDENTIFIER}'`,
+          )
+          if (helperFunc?.id?.name) {
+            helperFunctionName = helperFunc.id.name
+            walk(
+              ast,
+              (node) => {
+                if (isCallExpression(node) && node?.callee?.name === helperFunctionName) {
+                  if (node?.arguments?.length === 1 && isLiteral(node.arguments[0])) {
+                    const matchedPath = node.arguments[0].value
+                    taggedPaths.push({
+                      path: matchedPath,
+                      start: node.start,
+                      end: node.end,
+                    })
+                  } else {
+                    throw 'Arguments for VitePluginHelper should be just a plain String'
+                  }
+                }
+              },
+              undefined,
+              null,
+            )
+          }
+          return
+        }
+
+        // Find plain asset tags
         const matched = ASSET_TAG.exec(node.raw)
         if (matched !== null) {
           if (matched.groups?.path) {
-            taggedPaths[importNameFrom(matched.groups.path)] = {
+            taggedPaths.push({
               path: matched.groups.path,
               start: node.start,
               end: node.end,
-            }
+            })
           }
         }
       }
@@ -40,15 +108,17 @@ const assetsInjector = (compiledElmCodeInEsm: string) => {
     null,
   )
 
-  if (Object.keys(taggedPaths).length > 0) {
+  if (taggedPaths.length > 0) {
     const src = compiledElmCodeInEsm.split('')
     const importPaths: string[] = []
-    Object.entries(taggedPaths).forEach(([importName, { path, start, end }]) => {
-      importPaths.push(path)
+    taggedPaths.forEach(({ path, start, end }) => {
       for (let i = start; i < end; i++) {
         src[i] = ''
       }
-      src[start] = importName
+      src[start] = importNameFrom(path)
+      if (!importPaths.includes(path)) {
+        importPaths.push(path)
+      }
     })
     return `${generateImports(importPaths)}\n\n${src.join('')}`
   } else {
