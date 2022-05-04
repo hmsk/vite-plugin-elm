a874f2714abd4c3c54159cd44e14928cdfbf6b2a
hmsk <k.hamasaki@gmail.com>
2022-05-03 23:17:22 -0700

    add an example page of multi Elm files compilation

---
+++ example/src/multi.js
@@ -0,0 +1,4 @@
+import { Elm } from './Hello.elm?with=./Application.elm'
+
+
+Elm.Application.init()
--- example/vite.config.ts
+++ example/vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, 'index.html'),
         application: resolve(__dirname, 'application/index.html'),
+        multi: resolve(__dirname, 'multi/index.html'),
       },
     },
   },
