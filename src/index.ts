ab387839ba9f04d3e79082818aeeccc972b58d63
hmsk <k.hamasaki@gmail.com>
2022-05-05 20:05:07 -0700

    handle HMR for multiple input correctly

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,19 @@ import { acquireLock } from './mutex'
 const trimDebugMessage = (code: string): string => code.replace(/(console\.warn\('Compiled in DEBUG mode)/, '// $1')
 const viteProjectPath = (dependency: string) => `/${relative(process.cwd(), dependency)}`
 
+const parseImportId = (id: string) => {
+  const parsedId = new URL(id, 'file://')
+  const pathname = parsedId.pathname
+  const valid = pathname.endsWith('.elm')
+  const withParams = parsedId.searchParams.getAll('with')
+
+  return {
+    valid,
+    pathname,
+    withParams,
+  }
+}
+
 export const plugin = (opts?: { debug?: boolean; optimize?: boolean }): Plugin => {
   const compilableFiles: Map<string, Set<string>> = new Map()
   const debug = opts?.debug
@@ -22,7 +35,9 @@ export const plugin = (opts?: { debug?: boolean; optimize?: boolean }): Plugin =
     name: 'vite-plugin-elm',
     enforce: 'pre',
     handleHotUpdate({ file, server, modules }) {
-      if (!file.endsWith('.elm')) return
+      const { valid } = parseImportId(file)
+      if (!valid) return
+
       const modulesToCompile: ModuleNode[] = []
       compilableFiles.forEach((dependencies, compilableFile) => {
         if (dependencies.has(file)) {
@@ -43,36 +58,35 @@ export const plugin = (opts?: { debug?: boolean; optimize?: boolean }): Plugin =
       }
     },
     async load(id) {
-      const parsedId = new URL(id, 'file://')
-      if (!parsedId.pathname.endsWith('.elm')) return
-
-      const accompanyCandidates = parsedId.searchParams.getAll('with')
+      const { valid, pathname, withParams } = parseImportId(id)
+      if (!valid) return
 
       const accompanies = await (() => {
-        if (accompanyCandidates.length > 0) {
+        if (withParams.length > 0) {
           const importTree = this.getModuleIds()
           let importer = ''
           for (const moduleId of importTree) {
             if (moduleId === id) break
             importer = moduleId
           }
-          const resolveAcoompany = async (accompany: string) => (await this.resolve(accompany, importer))?.id
-          return Promise.all(accompanyCandidates.map(resolveAcoompany))
+          const resolveAcoompany = async (accompany: string) => (await this.resolve(accompany, importer))?.id ?? ''
+          return Promise.all(withParams.map(resolveAcoompany))
         } else {
           return Promise.resolve([])
         }
       })()
 
-      const targets = [parsedId.pathname, ...accompanies]
+      const targets = [pathname, ...accompanies].filter((target) => target !== '')
 
-      compilableFiles.delete(parsedId.pathname)
-
-      const isBuild = process.env.NODE_ENV === 'production'
-      const dependencies: string[] = await compiler.findAllDependencies(parsedId.pathname)
-      compilableFiles.set(parsedId.pathname, new Set(dependencies))
+      compilableFiles.delete(id)
+      const dependencies = (
+        await Promise.all<string[]>(targets.map((target) => compiler.findAllDependencies(target) as string[]))
+      ).flat()
+      compilableFiles.set(id, new Set([...accompanies, ...dependencies]))
 
       const releaseLock = await acquireLock()
       try {
+        const isBuild = process.env.NODE_ENV === 'production'
         const compiled: string = await compiler.compileToString(targets, {
           output: '.js',
           optimize: typeof optimize === 'boolean' ? optimize : !debug && isBuild,
@@ -80,12 +94,12 @@ export const plugin = (opts?: { debug?: boolean; optimize?: boolean }): Plugin =
           debug: debug ?? !isBuild,
         })
 
+        const esm = injectAssets(toESModule(compiled))
+
         // Apparently `addWatchFile` may not exist: https://github.com/hmsk/vite-plugin-elm/pull/36
         if (this.addWatchFile) {
           dependencies.forEach(this.addWatchFile.bind(this))
         }
-
-        const esm = injectAssets(toESModule(compiled))
 
         return {
           code: isBuild ? esm : trimDebugMessage(injectHMR(esm, dependencies.map(viteProjectPath))),
@@ -94,7 +108,7 @@ export const plugin = (opts?: { debug?: boolean; optimize?: boolean }): Plugin =
       } catch (e) {
         if (e instanceof Error && e.message.includes('-- NO MAIN')) {
           const message = `${viteProjectPath(
-            parsedId.pathname,
+            pathname,
           )}: NO MAIN .elm file is requested to transform by vite. Probably, this file is just a depending module`
           throw message
         } else {
