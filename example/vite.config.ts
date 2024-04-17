45186cb89317a0944e66be4716e357af94d97f78
hmsk <k.hamasaki@gmail.com>
2024-04-17 01:12:59 -0700

    try to use elm-optimize-level-2 in example

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -2,8 +2,13 @@ import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import plugin from 'vite-plugin-elm'
 
+const outputDir = process.env.GITHUB_WORKSPACE ?? __dirname
+const compileWithElmOptimizeLevel2 = (targets) => `npx elm-optimize-level-2 -O3 --output ${outputDir}/elm.js ${targets[0]} &> /dev/null && cat ${outputDir}/elm.js && rm -f ${outputDir}/elm.js &> /dev/null`;
+
 export default defineConfig({
-  plugins: [plugin()],
+  plugins: [plugin(
+    process.env.NODE_ENV === 'production' && !!process.env.COMMAND_LINE ? { compiler: { command: compileWithElmOptimizeLevel2 } } : {}
+  )],
   build: {
     rollupOptions: {
       input: {
